fe9675b173de5982770b5013f770fd3a40c1336b
kadolakatt <klacatt@uninorte.edu.co>
2021-10-13 21:01:03 -0500

    Componente Practico 14 (Sin env)

---
+++ cp14/forms.py
@@ -0,0 +1,34 @@
+from flask_wtf import FlaskForm
+from wtforms import validators
+from wtforms.fields.core import RadioField, StringField
+from wtforms.fields.simple import PasswordField, SubmitField, TextAreaField
+from wtforms.widgets.core import SubmitInput
+
+from models import usuario
+
+class FormLogin(FlaskForm):
+    usuario = StringField('Usuario', validators=[validators.required()])
+    contrasena = PasswordField('Contraseña',validators=[validators.required()])
+    enviar = SubmitField('Iniciar sesión')
+
+class FormContactanos(FlaskForm):
+    nombre = StringField('Nombre Completo', validators=[validators.required(), validators.length(max=100)]) 
+    correo = StringField('Correo Electrónico', validators=[validators.required(), validators.length(max=150)])
+    mensaje = TextAreaField('Mensaje', validators=[validators.required(), validators.length(max=500)])
+    tipo = RadioField('Tipo de Mensaje', choices=[('P','Pregunta'),('Q','Queja'),('S','Sugerencia')])
+    enviar = SubmitField('Enviar Mensaje')
+
+class FormRespuesta(FlaskForm):
+    nombre = StringField('Nombre Completo', validators=[validators.required(), validators.length(max=100)]) 
+    correo = StringField('Correo Electrónico', validators=[validators.required(), validators.length(max=150)])
+    mensaje_original = TextAreaField('Mensaje Original', validators=[validators.required(), validators.length(max=500)])
+    respuesta = TextAreaField('Respuesta', validators=[validators.required(), validators.length(max=500)])
+    enviar = SubmitField('Enviar Respuesta')
+
+#WTForms para registro de usuarios
+class FormRegistro(FlaskForm):
+    usuario = StringField('Usuario', validators=[validators.required(), validators.length(max=50) ])
+    nombre = StringField('Nombre Completo', validators=[validators.required(), validators.length(max=100)])
+    correo = StringField('Correo Electrónico', validators=[validators.email(), validators.length(max=150)])
+    contrasena = PasswordField('Contraseña', validators=[validators.required(), validators.length(max=20, min=8)])
+    enviar = SubmitField('Registrar')
